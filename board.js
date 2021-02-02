c4bdb4ae75747ecd57b1ef0e43827d53671b6e42
ShemarG <shemar.gordon32@gmail.com>
2021-02-02 11:58:29 -0500

    added win coditions

--- board.js
+++ board.js
@@ -1,41 +1,81 @@
 class Board {
-    constructor(table){
-        this.table = table;
-        this.boardState = [[],[],[]];
-        this.player = "O";
-        this.winner = null;
-    }
+  constructor(table) {
+    this.table = table;
+    this.boardState = [[], [], []];
+    this.player = 'O';
+    this.winner = null;
+  }
 
-    initBoard(){
-        const places = this.table.querySelectorAll("td");
-        places.forEach((place, ind) => {
-            if(this.boardState[0].length < 3){
-                this.boardState[0].push(place)
-            } else if(this.boardState[1].length < 3){
-                this.boardState[1].push(place);
-            } else if(this.boardState[2].length < 3){
-                this.boardState[2].push(place);
-            }
-            console.log(this.boardState);
-        });
-    }
+  initBoard() {
+    const places = this.table.querySelectorAll('td');
+    places.forEach((place, ind) => {
+      if (this.boardState[0].length < 3) {
+        this.boardState[0].push(place);
+      } else if (this.boardState[1].length < 3) {
+        this.boardState[1].push(place);
+      } else if (this.boardState[2].length < 3) {
+        this.boardState[2].push(place);
+      }
+      console.log(this.boardState);
+    });
+  }
 
-    changeTurn(){
-        if(player === "O"){
-            player = "X";
-        } else {
-            player = "O";
-        }
+  changeTurn() {
+    if (player === 'O') {
+      player = 'X';
+    } else {
+      player = 'O';
     }
+  }
 
-    updateBoard(e){
-        const places = this.table.querySelectorAll("td");
-        places.forEach(places => {
-            //update boardstate & table
-            // this.
-        });
-        this.changeTurn();
-    }
+  updateBoard(e) {
+    const places = this.table.querySelectorAll('td');
+    places.forEach((places) => {
+      // update boardstate & table
+      // this.
+    });
+    this.changeTurn();
+  }
 
-    checkWin(){};
+  checkWin() {}
 }
+
+// if (this.matrix[0][0] === this.matrix[0][1]
+//   && this.matrix[0][0] === this.matrix[0][2]
+//   && this.matrix[0][0] !== undefined) {
+//   this.renderWinner(this.elMatrix[0][0], this.elMatrix[0][1], this.elMatrix[0][2]);
+// } else if (this.matrix[1][0] === this.matrix[1][1]
+//   && this.matrix[1][0] === this.matrix[1][2]
+//   && this.matrix[1][0] !== undefined) {
+//   this.renderWinner(this.elMatrix[1][0], this.elMatrix[1][1], this.elMatrix[1][2]);
+// } else if (this.matrix[2][0] === this.matrix[2][1]
+//   && this.matrix[2][0] === this.matrix[2][2]
+//   && this.matrix[2][0] !== undefined) {
+//   this.renderWinner(this.elMatrix[2][0], this.elMatrix[2][1], this.elMatrix[2][2]);
+// } else if (this.matrix[0][0] === this.matrix[1][0]
+//   && this.matrix[0][0] === this.matrix[2][0]
+//   && this.matrix[0][0] !== undefined) {
+//   this.renderWinner(this.elMatrix[0][0], this.elMatrix[1][0], this.elMatrix[2][0]);
+// } else if (this.matrix[0][1] === this.matrix[1][1]
+//   && this.matrix[0][1] === this.matrix[2][1]
+//   && this.matrix[0][1] !== undefined) {
+//   this.renderWinner(this.elMatrix[0][1], this.elMatrix[1][1], this.elMatrix[2][1]);
+// } else if (this.matrix[0][2] === this.matrix[1][2]
+//   && this.matrix[0][2] === this.matrix[2][2]
+//   && this.matrix[0][2] !== undefined) {
+//   this.renderWinner(this.elMatrix[0][2], this.elMatrix[1][2], this.elMatrix[2][2]);
+// } else if (this.matrix[0][0] === this.matrix[1][1]
+//   && this.matrix[0][0] === this.matrix[2][2]
+//   && this.matrix[0][0] !== undefined) {
+//   this.renderWinner(this.elMatrix[0][0], this.elMatrix[1][1], this.elMatrix[2][2]);
+// } else if (this.matrix[0][2] === this.matrix[1][1]
+//   && this.matrix[0][2] === this.matrix[2][0]
+//   && this.matrix[0][2] !== undefined) {
+//   this.renderWinner(this.elMatrix[0][2], this.elMatrix[1][1], this.elMatrix[2][0]);
+// }
+// if (this.moveCount === 9 && this.winner === null) {
+//   this.winner = 'Draw';
+//   this.draws += 1;
+//   this.updateCounters();
+//   this.gameEnd();
+// }
